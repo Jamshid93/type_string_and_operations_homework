33082555992f579a5cf6e8fd28f0aa80a889ff42
Jamshid93 <hamzayevjf@gmail.com>
2022-10-28 16:48:39 +0500

    String 09 done

--- String09.py
+++ String09.py
@@ -8,7 +8,7 @@ def main(x1,x2,x3):
     Returns:
         str: return answer.
     """
-    answer=str(f'"[{(x1)}, {(x2)}, {(x3)}]"')
+    answer=str(f"[{(x1)}, {(x2)}, {(x3)}]")
     return answer
 print(main(1,2,3))
 
